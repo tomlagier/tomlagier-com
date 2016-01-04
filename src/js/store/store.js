469e6076141d19bcdc026a82b7ae676823131881
Tom Lagier <tom@rosetta.com>
2016-01-03 17:03:32 -0800

    More routing

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -2,29 +2,42 @@ import {createStore, combineReducers} from 'redux'
 import initialState from '../data/state'
 import { routeReducer, UPDATE_PATH } from 'redux-simple-router'
 
+function checkPortfolioState(path, state) {
+  if (path.indexOf('/portfolio') !== -1 ) {
+    state.portfolioTriggered = true
+  }
+
+  return state
+}
+
+function checkPortfolioModalState(path, state) {
+  let modalRegex = new RegExp('\/portfolio\/(.*)')
+
+  const matches = modalRegex.exec(path)
+
+  let slug
+  if (matches) {
+    slug = matches[1]
+  }
+
+  if (slug) {
+    state.currentModal = slug
+  } else {
+    state.currentModal = -1
+  }
+  return state
+}
+
+initialState = checkPortfolioState(window.location.pathname, initialState)
+initialState = checkPortfolioModalState(window.location.pathname, initialState)
+
 function appState(state = initialState, action) {
   let localState = Object.assign({}, state)
 
   switch (action.type) {
     case UPDATE_PATH:
-      if (action.payload.path.indexOf('/portfolio') !== -1 ) {
-        localState.portfolioTriggered = true
-      }
-
-      let modalRegex = new RegExp('\/portfolio\/(.*)')
-
-      const matches = modalRegex.exec(action.payload.path)
-
-      let slug
-      if (matches) {
-        slug = matches[1]
-      }
-
-      if (slug) {
-        localState.currentModal = slug
-      } else {
-        localState.currentModal = -1
-      }
+      localState = checkPortfolioState(action.payload.path, localState)
+      localState = checkPortfolioModalState(action.payload.path, localState)
       return localState
 
     case 'ACTIVATE_CONTENT':
